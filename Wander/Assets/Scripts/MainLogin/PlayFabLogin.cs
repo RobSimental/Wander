a4d2f7fe0c140aa1be1c8c6b6c15e2727b1bb31c
Roberto Simental <60452727+RobSimental@users.noreply.github.com>
2021-03-23 01:37:55 -0700

    saved player preferences locally
    allows for quickly logging in for development purposes

--- Wander/Assets/Scripts/MainLogin/PlayFabLogin.cs
+++ Wander/Assets/Scripts/MainLogin/PlayFabLogin.cs
@@ -6,9 +6,15 @@ using PlayFab.ClientModels;
 using PlayFab.ServerModels;
 using Mirror;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class PlayFabLogin : NetworkBehaviour
 {
+
+    public GameObject emailInput;
+    public GameObject passwordInput;
+    private TMP_InputField emailTextField;
+    private TMP_InputField passwordTextField;
     private string userEmail;
     private string userPassword;
     public static string sessionTicket; //lets you know what player you are in server
@@ -19,6 +25,16 @@ public class PlayFabLogin : NetworkBehaviour
         {
             //set the title id to the Wander playfab title id
             PlayFabSettings.TitleId = "23BBA";
+        }
+        emailTextField = emailInput.GetComponent<TMP_InputField>();
+        passwordTextField = passwordInput.GetComponent<TMP_InputField>();
+        //get player preferences for easy login
+        if (PlayerPrefs.HasKey("EMAIL"))
+        {
+            emailTextField.text = PlayerPrefs.GetString("EMAIL");
+            passwordTextField.text = PlayerPrefs.GetString("PASSWORD");
+            //emailTextField.SetText(PlayerPrefs.GetString("EMAIL"));
+            //emailTextField.text = PlayerPrefs.GetString("EMAIL");
         }
     } 
 
@@ -38,6 +54,9 @@ public class PlayFabLogin : NetworkBehaviour
     {
         Debug.Log("logged in");
         sessionTicket = result.SessionTicket;
+        //save player preferences locally
+        PlayerPrefs.SetString("EMAIL", userEmail);
+        PlayerPrefs.SetString("PASSWORD", userPassword);
         SceneManager.LoadScene("KeyToContinue");
     }
     private void OnLoginFailure(PlayFabError error)
