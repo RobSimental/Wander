e713492186d4cd87f8a48834e829165313395268
Roberto Simental <60452727+RobSimental@users.noreply.github.com>
2021-03-28 00:17:38 -0700

    added physics to potions
    potion jumps then falls

--- Wander/Assets/Scripts/Player/AddItem.cs
+++ Wander/Assets/Scripts/Player/AddItem.cs
@@ -6,21 +6,27 @@ using PlayFab;
 using PlayFab.ServerModels;
 public class AddItem : NetworkBehaviour
 {
+    public LayerMask ground;
+    private Transform groundCheck;
     private Rigidbody2D rb;
+    private Collider2D collider;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.up * 10f);
     }
+   
  
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            Player player = collision.gameObject.GetComponent<Player>();
+            Player player = collision.gameObject.GetComponent<Player>();           
             player.CmdAddItemToUser(this.gameObject.name);
             Destroy(this.gameObject);
         }
     }
+  
 
 }
